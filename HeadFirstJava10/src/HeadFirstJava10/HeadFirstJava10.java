679e98e26233d7718b02b5a970f419631869b2ee
unknown <A12710@PC-7940.cyberagent.co.jp>
2012-10-04 11:27:55 +0900

    Math

--- HeadFirstJava10/src/HeadFirstJava10/HeadFirstJava10.java
+++ HeadFirstJava10/src/HeadFirstJava10/HeadFirstJava10.java
@@ -2,10 +2,30 @@ package HeadFirstJava10;
 
 public class HeadFirstJava10 {
 
+	//Mathのコンストラクタがprivate宣言されているのでエラー
+	//Math mathObject = new Math();
 
 	public void mathTest() {
 
-		long x = Math.round(42.4);
+		/*
+		 * Mathクラスメソッド
+		 */
+
+		//0.0 ~ 0.1の範囲の無作為の数値を戻す
+		Math.random();
+
+		//絶対値を返す
+		Math.abs(-243);
+
+		//整数を返す float型なら
+		int x = Math.round(42.4f);
+
+		//小数値を返す
+		Math.min(24, 240);
+
+		//最大値を返す
+		Math.max(24, 240);
+
 	}
 
 }
