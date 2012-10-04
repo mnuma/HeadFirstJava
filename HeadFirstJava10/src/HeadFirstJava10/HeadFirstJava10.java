1653e0f81778b50cd9e8c266e9b7c7689ee442d4
unknown <A12710@PC-7940.cyberagent.co.jp>
2012-10-04 11:18:00 +0900

    add test

---
+++ HeadFirstJava10/src/HeadFirstJava10/HeadFirstJava10.java
@@ -0,0 +1,11 @@
+package HeadFirstJava10;
+
+public class HeadFirstJava10 {
+
+
+	public void mathTest() {
+
+		long x = Math.round(42.4);
+	}
+
+}
---
+++ HeadFirstJava10/src/HeadFirstJava10/test/HeadFirstJava10Test.java
@@ -0,0 +1,18 @@
+package HeadFirstJava10.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import HeadFirstJava10.HeadFirstJava10;
+
+public class HeadFirstJava10Test {
+
+	HeadFirstJava10 headFirstJava10 = new HeadFirstJava10();
+
+	@Test
+	public void mathTestTest() {
+		System.out.println();
+	}
+
+}
